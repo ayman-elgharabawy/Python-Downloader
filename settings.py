8db00bf85e7ce96786824aea24b4dfbbc209ea5d
ayman Elgharabawy <ayman.elgharabawy@vodafone.com>
2016-07-07 21:42:03 +0200

    enhance

--- settings.py
+++ settings.py
@@ -64,3 +64,9 @@ def init():
     templist=[]
     global download_counter
     download_counter=0
+    global no_thread_link
+    global no_thread_per_file
+    no_thread_link=4
+    no_thread_per_file=4 
+    global shared_bytes_var
+    shared_bytes_var = multiprocessing.Value(c_int, 0) # a ctypes var that counts the bytes already downloaded
