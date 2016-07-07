97d5d439aa367c1dcda045afec9cb34a958977ef
ayman Elgharabawy <ayman.elgharabawy@vodafone.com>
2016-07-07 21:52:45 +0200

    oranize import

--- settings.py
+++ settings.py
@@ -4,25 +4,10 @@ Created on July 7, 2016
 
 @author: Ayman Elgharabawy
 '''
-from multiprocessing import Pool, TimeoutError
-from StringIO import StringIO
 import time
 import os
-import urllib2
-import shutil
 import multiprocessing
-import string
-from random import choice
-import socket
 from ctypes import c_int
-import tempfile
-import thread
-from time import sleep
-from Queue import Queue
-from threading import Thread
-import itertools
-from multiprocessing import Pool
-import logging 
 import types
 import multiprocessing
 
