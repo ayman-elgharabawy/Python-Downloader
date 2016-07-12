1ecfdbc51873c798cd65be3156ecc85814fcc736
Ayman Elgharabawy <aaaeg@hotmail.com>
2016-07-12 07:52:42 +0200

    Add proxy connection

--- settings.py
+++ settings.py
@@ -14,6 +14,12 @@ import urllib2
 
 
 def init():
+    global proxyenable
+    proxyenable=False
+    global proxy_username
+    global proxy_password
+    global proxy_host
+    global proxy_port
     global status
     status=''
     global DownloadedFolder
@@ -27,4 +33,11 @@ def init():
     no_thread_link=4
     no_thread_per_file=4 
     global shared_bytes_var
+    ##############################################Init Methods ########################################
     shared_bytes_var = multiprocessing.Value(c_int, 0) # a ctypes var that counts the bytes already downloaded
+    if proxyenable:
+        proxy_handler = urllib2.ProxyHandler({'http': proxy_host+':'+proxy_port+'/'})
+        proxy_auth_handler = urllib2.ProxyBasicAuthHandler()
+        proxy_auth_handler.add_password('realm', proxy_host, proxy_username, proxy_password)
+        opener = urllib2.build_opener(proxy_handler)
+        urllib2.install_opener(opener)    
